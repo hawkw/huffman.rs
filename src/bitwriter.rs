a41e2b2bcea043330afb9b187952ad5902c281a3
Hawk Weisman <hi@hawkweisman.me>
2016-03-28 17:30:13 -0400

    bitwriter.rs: BitWriter now implements io::Write

--- src/bitwriter.rs
+++ src/bitwriter.rs
@@ -10,57 +10,55 @@ where W: Write
             }
 
 impl<'a, W: Write> BitWriter<'a, W> {
+
     pub fn new(w: &'a mut W) -> BitWriter<'a, W> {
         BitWriter { out: w, buf: 0, n: 0 }
     }
 
-    pub fn flush(&mut self) -> Result<()> {
-        self.empty_buf();
-        self.out.flush()
-    }
-
-    #[inline] fn empty_buf(&mut self) {
+    #[inline] fn empty_buf(&mut self) -> Result<usize> {
         let byte = &[self.buf << (8 - self.n)];
         self.n = 0;
         self.buf = 0;
         self.out.write(byte)
-            .expect("Could not flush buffer!");
     }
 
-    pub fn write_bit(&mut self, bit: bool) {
+    pub fn write_bit(&mut self, bit: bool) -> Result<usize> {
         self.buf <<= 1;
         if bit { self.buf |= 1; }
         self.n += 1;
         if self.n == 8 {
             self.empty_buf()
-        }
-    }
-
-    pub fn write_byte(&mut self, byte: u8) {
-        if self.n == 0 {
-            // if we're aligned on a byte boundary we can just write the byte
-            self.out.write(&[byte])
-                .expect("Could not write byte!");
         } else {
-            // otherwise, we have to write out the byte one bit at a time
-            for bit in 0..8 {
-                self.write_bit((1 << bit) & byte == 1);
-            }
+            Ok(0)
         }
     }
 
-    pub fn write_bytes(&mut self, bytes: &[u8]) {
+    #[inline] pub fn write_byte(&mut self, byte: u8) -> Result<usize> {
+        self.write(&[byte])
+    }
+}
+
+impl<'a, W: Write> Write for BitWriter<'a, W> {
+
+    fn write(&mut self, bytes: &[u8]) -> Result<usize> {
         if self.n == 0 {
             // if we're aligned on a byte boundary we can just write the byte
             self.out.write(bytes)
-                .expect("Could not write bytes!");
         } else {
             // otherwise, we have to write out the byte one bit at a time
+            let mut written = 0;
             for byte in bytes {
                 for bit in 0..8 {
-                    self.write_bit((1 << bit) & byte == 1);
+                    written += try!(self.write_bit((1 << bit) & byte == 1))
                 }
             }
+            Ok(written)
         }
     }
+
+    fn flush(&mut self) -> Result<()> {
+        self.empty_buf()
+            .and_then(|_| self.out.flush())
+    }
+
 }
